7cc054f07038e663e5af94c330547bdebcf86ab0
hanhoseok <mrhanhoseok@gmail.com>
2026-05-05 08:54:42 +0900

    FEAT: 리뷰 작성 구현

--- src/main/java/com/example/umc_study/domain/review/controller/ReviewController.java
+++ src/main/java/com/example/umc_study/domain/review/controller/ReviewController.java
@@ -1,4 +1,30 @@
 package com.example.umc_study.domain.review.controller;
 
+
+import com.example.umc_study.domain.review.dto.ReviewReqDTO;
+import com.example.umc_study.domain.review.dto.ReviewResDTO;
+import com.example.umc_study.domain.review.exception.code.ReviewSuccessCode;
+import com.example.umc_study.domain.review.service.ReviewService;
+import com.example.umc_study.global.apiPayload.ApiResponse;
+import com.example.umc_study.global.code.BaseSuccessCode;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api")
 public class ReviewController {
+
+    private final ReviewService reviewService;
+
+    @PostMapping("/{storeId}/reviews")
+    public ApiResponse<ReviewResDTO.CreateResultDTO> createReview(
+            @PathVariable(name = "storeId") Long storeId,
+            @RequestHeader(name = "Authorization") String token,
+            @RequestBody ReviewReqDTO.CreateReviewDTO request
+    ) {
+        ReviewResDTO.CreateResultDTO result = reviewService.createReview(storeId, request);
+
+        return ApiResponse.onSuccess(ReviewSuccessCode.OK, result);
+    }
 }
--- src/main/java/com/example/umc_study/domain/review/dto/ReviewReqDTO.java
+++ src/main/java/com/example/umc_study/domain/review/dto/ReviewReqDTO.java
@@ -1,4 +1,13 @@
 package com.example.umc_study.domain.review.dto;
 
+import lombok.Getter;
+
 public class ReviewReqDTO {
+
+    @Getter
+    public static class CreateReviewDTO {
+        private Double score;    // 소수점 별점
+        private String content;  // 리뷰 내용
+        private String image;    // 이미지 URL
+    }
 }
