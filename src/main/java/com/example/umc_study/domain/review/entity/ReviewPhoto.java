ef5dd4edbeed0dfb120a8c1811183bfce8d1970d
hanhoseok <mrhanhoseok@gmail.com>
2026-05-06 10:42:36 +0900

    FEAT: Review 엔티티 생성 및 매핑

--- src/main/java/com/example/umc_study/domain/review/entity/ReviewPhoto.java
+++ src/main/java/com/example/umc_study/domain/review/entity/ReviewPhoto.java
@@ -1,4 +1,25 @@
 package com.example.umc_study.domain.review.entity;
 
-public class ReviewPhoto {
+import com.example.umc_study.global.common.BaseEntity;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Getter
+@Table(name = "review_photo")
+public class ReviewPhoto extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, columnDefinition = "TEXT")
+    private String imageUrl;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "review_id")
+    private Review review;
 }
